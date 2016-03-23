70b070928878a1bfa4618af8b9b6d4e3bfe7fc3c
Jan Mosat <jmosat@gmail.com>
2016-03-23 10:06:11 +0100

    [MOD] code cleanup

--- lib/download_headers.rb
+++ lib/download_headers.rb
@@ -3,16 +3,21 @@ require 'open-uri'
 require 'browser'
 
 class DownloadHeaders
+  WEB = {
+    :url              => 'http://www.webuseragents.com/recent',
+    :browser_path     => '//div[@id="content"]/div[@style="margin-left:30px;"]',
+    :date_path        => './/span[@class="small"]',
+    :user_agent_path  => './/a'
+  }
   class << self
     def add_headers
       update = DB[:Headers].order(:date).select(:date).last
       last_update = update ? update.values.first : Date.new
-      doc = Nokogiri::HTML(open('http://www.webuseragents.com/recent'))
-      browsers_data = doc.xpath('//div[@id="content"]
-        /div[@style="margin-left:30px;"]')
+      doc = Nokogiri::HTML(open(WEB[:url]))
+      browsers_data = doc.xpath(WEB[:browser_path])
       browsers_data.reverse_each do |browser|
-        user_agent = browser.at_xpath('.//a').content.strip
-        date = clean_date(browser.at_xpath('.//span[@class="small"]').content.strip)
+        user_agent = browser.at_xpath(WEB[:user_agent_path]).content.strip
+        date = clean_date(browser.at_xpath(WEB[:date_path]).content.strip)
         if date >= last_update && accepted_header?(user_agent)
           DB[:Headers].insert_ignore.insert(:header => user_agent, :date => date)
         end
@@ -24,9 +29,9 @@ class DownloadHeaders
       Date.strptime(parsed, 'On %A the %d of %B, AD %Y')
     end
 
-    def accepted_header?(browser)
-      b = Browser.new(user_agent)
-      accepted_browser?(b.browser) && accepted_os?(b.browser)
+    def accepted_header?(user_agent)
+      browser = Browser.new(user_agent)
+      accepted_browser?(browser) && accepted_os?(browser)
     end
 
     def accepted_browser?(browser)
